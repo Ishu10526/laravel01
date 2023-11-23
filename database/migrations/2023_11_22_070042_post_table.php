59011b24235cd76e2bfd7c45fceec377d89ede88
aswal94 <aswal.amit94@gmail.com>
2023-11-23 22:00:41 +0530

    Updated table name to support laravel convention

--- database/migrations/2023_11_22_070042_post_table.php
+++ database/migrations/2023_11_22_070042_post_table.php
@@ -10,7 +10,7 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
             $table->string('title');
